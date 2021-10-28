a31433a25d61b5f5f9b23a765176855c7de10b55
juliuswiscmsba <mlee439@wisc.edu>
2021-10-27 22:54:08 -0500

    Part A with Read Json File Function

--- server.py
+++ server.py
@@ -2,17 +2,18 @@ import requests
 import os
 import json
 import time
+import sys
 
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 bearer_token = os.environ.get("BEARER_TOKEN")
 
-
+# imput Twitter Api
 def create_url():
     url = "https://api.twitter.com/2/tweets/sample/stream?tweet.fields=created_at%2Clang"
     return url
 
-
+#Key authorization
 def bearer_oauth(r):
     """
     Method required by bearer token authentication.
@@ -22,7 +23,7 @@ def bearer_oauth(r):
     r.headers["User-Agent"] = "v2SampledStreamPython"
     return r
 
-
+#Copy the English text and date of each tweets into tweets.text 
 def connect_to_endpoint(url,f):
     response = requests.request("GET", url, auth=bearer_oauth, stream=True)
     print(response.status_code)
@@ -32,11 +33,11 @@ def connect_to_endpoint(url,f):
             if json_response["data"]["lang"] == "en":
             	json_response["data"]["created_at"] = time.strftime("%Y-%m-%d-%H-%M-%S", time.strptime(json_response["data"]["created_at"][:19], "%Y-%m-%dT%H:%M:%S"))
             	new_json_response = json_response["data"]["text"]+"    "+json_response["data"]["created_at"]+"\n"
-            	#from: https://stackoverflow.com/questions/214777/how-do-you-convert-yyyy-mm-ddthhmmss-000z-time-format-to-mm-dd-yyyy-time-forma/215313
+            	#From: https://stackoverflow.com/questions/214777/how-do-you-convert-yyyy-mm-ddthhmmss-000z-time-format-to-mm-dd-yyyy-time-forma/215313
             	
             	f.write(new_json_response)
 
-            	#print(new_json_response)
+            	print(new_json_response)
             	
             	
     if response.status_code != 200:
@@ -54,7 +55,25 @@ def main():
     while True:
         connect_to_endpoint(url,f)
         timeout += 1
-    
+
+
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 1:
+     with open(sys.argv[1], 'r') as fjson:
+      contents = fjson.read()
+      f = open('tweets.txt','w')
+      f.write(contents)
+      #From: https://stackoverflow.com/questions/7439145/i-want-to-read-in-a-file-from-the-command-line-in-python/7439162?fbclid=IwAR3kjvB_uua5NZo1qiSEWSqXYAMgG7a3Y2ynbe14o2Js2JKaps49pA_fTqM
+    else:
+     main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
