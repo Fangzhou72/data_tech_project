1618cb83bcfc57154d1cc10b64b5db679888af40
Mil-Wang <90532579+Mil-Wang@users.noreply.github.com>
2021-11-30 11:26:35 -0600

    Milestone2 Part A update

--- schema_postgres.sql
+++ schema_postgres.sql
@@ -1,19 +1,22 @@
+DROP TABLE IF EXISTS WORD_STREAM;
+DROP TABLE IF EXISTS MINUTE_SUMMARY;
+DROP TABLE IF EXISTS WORD_TRENDINESS;
+
 CREATE TABLE WORD_STREAM (
-  stream_id int Primary Key, 
+  stream_id SERIAL PRIMARY KEY, 
   word varchar(255) NOT NULL, 
-  timestamp datetime NOT NULL, 
-  minute datetime NOT NULL
+  timestamp varchar(50) NOT NULL
 );
 
 CREATE TABLE MINUTE_SUMMARY (
-  minute datetime Primary Key,
+  minute varchar(50) Primary Key,
   total_word_count int NOT NULL, 
   vocab_size int NOT NULL
 );
 
 CREATE TABLE WORD_TRENDINESS (
   word varchar(255) NOT NULL, 
-  minute datetime NOT NULL,
+  minute varchar(50) NOT NULL,
   word_count int NOT NULL, 
   trendiness int NOT NULL,
   Primary Key (word, minute)
